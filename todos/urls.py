4ef2480b06bf9713525f45b944544af57ae07c42
phil sobrepena <JPLS791@gmail.com>
2023-05-31 16:05:14 -0700

    Feature 9 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from todos.views import TodoList_view, TodoList_Details
+from todos.views import TodoList_view, TodoList_Details, TodoList_Create
 
 urlpatterns = [
+    path("create/", TodoList_Create, name="todo_list_create"),
     path("<int:id>/", TodoList_Details, name="todo_list_detail"),
     path("", TodoList_view, name="todo_list_list"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from todos.models import TodoList, TodoItem
+from todos.forms import TodoForm
 
 
 # Create your views here.
@@ -19,3 +20,16 @@ def TodoList_Details(request, id):
         "todo_list": todo_list,
     }
     return render(request, "todos/details.html", context)
+
+
+def TodoList_Create(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todolist = form.save()
+            return redirect("todo_list_list")
+    else:
+        form = TodoForm()
+    context = {"form": form}
+
+    return render(request, "todos/create.html", context)
