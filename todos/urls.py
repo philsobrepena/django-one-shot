76a6bf062628cd15506b46c8ea592e77215f3ee2
phil sobrepena <JPLS791@gmail.com>
2023-05-31 17:11:50 -0700

    Feature 11 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from todos.views import TodoList_view, TodoList_Details, TodoList_Create
+from todos.views import (
+    TodoList_view,
+    TodoList_Details,
+    TodoList_Create,
+    TodoList_Update,
+    TodoList_Delete,
+)
 
 urlpatterns = [
+    path("<int:id>/delete/", TodoList_Delete, name="todo_list_delete"),
+    path("<int:id>/edit/", TodoList_Update, name="todo_list_update"),
     path("create/", TodoList_Create, name="todo_list_create"),
     path("<int:id>/", TodoList_Details, name="todo_list_detail"),
     path("", TodoList_view, name="todo_list_list"),
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from todos.models import TodoList, TodoItem
+from todos.models import TodoList
 from todos.forms import TodoForm
 
 
@@ -26,10 +26,35 @@ def TodoList_Create(request):
     if request.method == "POST":
         form = TodoForm(request.POST)
         if form.is_valid():
-            todolist = form.save()
+            form.save()
             return redirect("todo_list_list")
     else:
         form = TodoForm()
     context = {"form": form}
 
     return render(request, "todos/create.html", context)
+
+
+def TodoList_Update(request, id):
+    todolist = get_object_or_404(TodoList, id=id)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todolist)
+        if form.is_valid():
+            form.save()
+            return redirect("todo_list_detail", id=id)
+    else:
+        form = TodoForm(instance=todolist)
+    context = {
+        "todolist": todolist,
+        "form": form,
+    }
+
+    return render(request, "todos/update.html", context)
+
+
+def TodoList_Delete(request, id):
+    todolist = get_object_or_404(TodoList, id=id)
+    if request.method == "POST":
+        todolist.delete()
+        return redirect("todo_list_list")
+    return render(request, "todos/delete.html")
